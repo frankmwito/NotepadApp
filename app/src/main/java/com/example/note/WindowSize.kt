78bd320fdf4cd638ff839e6d64db0fe425e718e1
Frank Mwito <Mfranklyne039@gmail.com>
2024-04-08 21:30:41 +0300

    UserInterface

---
+++ app/src/main/java/com/example/note/WindowSize.kt
@@ -0,0 +1,29 @@
+package com.example.note
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+
+data class WindowSize(
+    val width: WindowType,
+    val height: WindowType
+)
+enum class WindowType{
+    Compact, Medium, Expanded
+}
+
+@Composable
+fun rememberWindowSize(): WindowSize {
+    val configuration = LocalConfiguration.current
+    return WindowSize(
+        width = when {
+            configuration.smallestScreenWidthDp < 600 -> WindowType.Compact
+            configuration.screenWidthDp < 840 -> WindowType.Medium
+            else -> WindowType.Expanded
+        },
+        height = when {
+            configuration.screenHeightDp < 600 -> WindowType.Compact
+            configuration.screenHeightDp < 840 -> WindowType.Medium
+            else -> WindowType.Expanded
+        }
+    )
+}
