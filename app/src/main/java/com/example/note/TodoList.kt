5f23097ab74026e5d4ef22c4951ad97266878393
Frank Mwito <Mfranklyne039@gmail.com>
2024-05-17 17:08:32 +0300

    UserInterface

--- app/src/main/java/com/example/note/TodoList.kt
+++ app/src/main/java/com/example/note/TodoList.kt
@@ -4,20 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Card
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class TodoList: ComponentActivity() {
@@ -29,35 +29,32 @@ class TodoList: ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Todolist() {
-        Surface(
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "To-Do-List",
+                    fontSize = 24.sp,
+                    color = Color.Black,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.SansSerif) }
+            )
+        },
+    ) {paddingValues -> paddingValues
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(color = Color.White)
+                .padding(16.dp)
         ) {
-            Box {
-                Row(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Card(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = Color.Gray)
-                    ) {
-                        Text(
-                            text = "Welcome to TodoListScreen",
-                            color = Color.Black,
-                            fontStyle = FontStyle.Italic,
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold,
-                            fontFamily = FontFamily.SansSerif
-                        )
-                    }
-                }
-
-            }
+           Text(text = "welcome",
+               color = Color.Black)
         }
+    }
 }
+
