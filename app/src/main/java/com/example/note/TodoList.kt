c69a9f9dd51a63c38d3335322d2b4b596be21a66
Frank Mwito <Mfranklyne039@gmail.com>
2024-05-20 21:50:43 +0300

    UserInterface

--- app/src/main/java/com/example/note/TodoList.kt
+++ app/src/main/java/com/example/note/TodoList.kt
@@ -1,24 +1,29 @@
 package com.example.note
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Sort
+import androidx.compose.material.icons.filled.Task
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -53,35 +58,59 @@ fun Todolist() {
             .background(color = Color.White),
         topBar = {
             TopAppBar(
-                title = { Text(text = "To-Do-List",
-                    fontSize = 24.sp,
-                    color = Color.Black,
-                    fontStyle = FontStyle.Italic,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = FontFamily.SansSerif)
+                title = {
+                    Text(
+                        text = "To-Do-List",
+                        fontSize = 24.sp,
+                        color = Color.Black,
+                        fontStyle = FontStyle.Italic,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif
+                    )
                 },
                 actions = {
                     IconButton(onClick = { /*TODO*/ }) {
-                        Icon(imageVector = Icons.Default.Search,
-                            contentDescription = "Sort")
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Sort"
+                        )
                     }
                     IconButton(onClick = { /*TODO*/ }) {
-                        Icon(imageVector = Icons.Default.Sort,
-                            contentDescription = "Search")
+                        Icon(
+                            imageVector = Icons.Default.Sort,
+                            contentDescription = "Search"
+                        )
                     }
                 }
             )
         }
-    ) {paddingValues -> paddingValues
-        Column(
+    ) { paddingValues ->
+        paddingValues
+        val ctx = LocalContext.current
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
+                .padding(bottom = 75.dp), // Add padding to keep FAB above navigation bar
+            contentAlignment = Alignment.BottomEnd
         ) {
-           Text(text = "welcome",
-               color = Color.Black)
+            ExtendedFloatingActionButton(
+                text = { Text(text = "New Task", color = Color.Black) },
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Task,
+                        contentDescription = "Create a new Task",
+                        tint = Color.Black
+                    )
+                },
+                onClick = {
+                    val intent = Intent(ctx, Noteadd::class.java)
+                    ctx.startActivity(intent)
+                },
+                containerColor = Color(0xFFCCC2DC),
+                modifier = Modifier.padding(16.dp)
+            )
         }
     }
-
 }
+
 
