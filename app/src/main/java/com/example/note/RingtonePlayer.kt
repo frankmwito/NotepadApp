eaf548e41343d7dd98134e76cea944fb244ee390
Frank Mwito <Mfranklyne039@gmail.com>
2024-05-29 23:06:43 +0300

    UserInterface

---
+++ app/src/main/java/com/example/note/RingtonePlayer.kt
@@ -0,0 +1,32 @@
+package com.example.note
+
+import android.content.Context
+import android.media.MediaPlayer
+import android.net.Uri
+
+object RingtonePlayer {
+    private var mediaPlayer: MediaPlayer? = null
+
+    fun play(context: Context, uri: Uri) {
+        stop() // Stop any currently playing ringtone
+        mediaPlayer = MediaPlayer().apply {
+            setDataSource(context, uri)
+            isLooping = true // Set looping to true
+            setOnPreparedListener {
+                it.start()
+            }
+            prepareAsync() // Prepare the MediaPlayer asynchronously
+        }
+    }
+
+    fun stop() {
+        mediaPlayer?.let {
+            if (it.isPlaying) {
+                it.stop()
+            }
+            it.release()
+        }
+        mediaPlayer = null
+    }
+}
+
