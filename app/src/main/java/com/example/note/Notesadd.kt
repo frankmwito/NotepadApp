6d848bd93b202a4f0c69617b41f51be2f63c6ece
Frank Mwito <Mfranklyne039@gmail.com>
2024-06-03 22:23:23 +0300

    UserInterface

--- app/src/main/java/com/example/note/Notesadd.kt
+++ app/src/main/java/com/example/note/Notesadd.kt
@@ -2,35 +2,45 @@ package com.example.note
 
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.SaveAlt
 import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -59,8 +69,10 @@ class Noteadd : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NotesAdd(viewModel: NotesViewModel, onBackPressed: () -> Unit) {
+    val context = LocalContext.current
     val title = remember { mutableStateOf("") }
     val body = remember { mutableStateOf("") }
+    val category = remember { mutableStateOf("Journal") } // Default category
 
     Scaffold(
         topBar = {
@@ -85,8 +97,16 @@ fun NotesAdd(viewModel: NotesViewModel, onBackPressed: () -> Unit) {
                 },
                 actions = {
                     IconButton(onClick = {
-                        viewModel.insertNote(title = title.value, body = body.value)
-                        onBackPressed()
+                        if (title.value.isBlank() || category.value.isBlank()) {
+                            Toast.makeText(context, "Title and Category cannot be empty", Toast.LENGTH_SHORT).show()
+                        } else {
+                            viewModel.insertNote(
+                                title = title.value,
+                                body = body.value,
+                                category = category.value
+                            )
+                            onBackPressed()
+                        }
                     }) {
                         Icon(
                             imageVector = Icons.Filled.SaveAlt,
@@ -95,20 +115,26 @@ fun NotesAdd(viewModel: NotesViewModel, onBackPressed: () -> Unit) {
                     }
                 }
             )
+        },
+        content = {
+                innerPadding -> 
+                Column(
+                    modifier = Modifier.padding(innerPadding)
+                ){
+                    CategorySelection(category)
+                    BodyContent(title = title, body = body, category = category)
+                }
+            
         }
-    ) { PaddingValues -> PaddingValues
-        BodyContent(title = title, body = body)
-    }
+    )
 }
 
-
 @Composable
-fun BodyContent(title: MutableState<String>, body: MutableState<String>) {
+fun BodyContent(title: MutableState<String>, body: MutableState<String>, category: MutableState<String>) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
-            .offset(y = 70.dp)
     ) {
         TextField(
             value = title.value,
@@ -134,10 +160,7 @@ fun BodyContent(title: MutableState<String>, body: MutableState<String>) {
             TextField(
                 value = body.value,
                 onValueChange = { body.value = it },
-                label = { Text(text = "Body",fontSize = 16.sp,
-                    color = Color.Black,
-                    fontStyle = FontStyle.Italic,
-                    fontFamily = FontFamily.SansSerif)},
+                label = { Text(text = "Body", fontSize = 16.sp, color = Color.Black, fontStyle = FontStyle.Italic, fontFamily = FontFamily.SansSerif) },
                 placeholder = {
                     Text(
                         text = "Note something down.....",
@@ -147,9 +170,53 @@ fun BodyContent(title: MutableState<String>, body: MutableState<String>) {
                         fontFamily = FontFamily.SansSerif
                     )
                 },
-                modifier = Modifier.fillMaxWidth(),
-
-                )
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
+
+@Composable
+fun CategorySelection(category: MutableState<String>) {
+    val categories = listOf("Journals", "Notes")
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(8.dp),
+    ) {
+        TextButton(onClick = { expanded = true }) {
+            Text(
+                text = "category",
+                fontSize = 16.sp,
+                color = Color.Black,
+                fontStyle = FontStyle.Italic,
+                fontFamily = FontFamily.SansSerif
+            )
+            Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Toggle Dropdown")
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.width(IntrinsicSize.Max)
+        ) {
+            categories.forEach { categoryText ->
+                DropdownMenuItem(onClick = {
+                    category.value = categoryText
+                    expanded = false
+                }) {
+                    Text(
+                        text = categoryText,
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        fontStyle = FontStyle.Italic,
+                        fontFamily = FontFamily.SansSerif
+                    )
+                }
+            }
+        }
+    }
+}
+
+
